2a19edaadf14b1ef47267800441d0070e2d7d250
ddusi <knormla76@naver.com>
2019-12-24 15:55:31 +0900

    20191224 // Spring_boot01

--- Spring/basic/src/main/java/com/ddusi/basic/BasicApplication.java
+++ Spring/basic/src/main/java/com/ddusi/basic/BasicApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicApplication {
-//Ctrl + f11
+//Ctrl + f11 : 실행 할 수 있는 것 실행 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicApplication.class, args);
 	}
-
 }
